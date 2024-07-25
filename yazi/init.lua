159c2dbbd6e8444e1b39ee043a49578814011b4c
aafrus <123903734+aafrus@users.noreply.github.com>
2024-07-25 13:40:22 +0200

    Update init.lua

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,26 +1,5 @@
 ---@diagnostic disable: undefined-global
 
--- Draw a full bar around window
-function Manager:render(area)
-	self.area = area
-
-	local chunks = ui.Layout()
-		:direction(ui.Layout.HORIZONTAL)
-		:constraints({
-			ui.Constraint.Ratio(MANAGER.ratio.parent, MANAGER.ratio.all),
-			ui.Constraint.Ratio(MANAGER.ratio.current, MANAGER.ratio.all),
-			ui.Constraint.Ratio(MANAGER.ratio.preview, MANAGER.ratio.all),
-		})
-		:split(area)
-
-  local bar = function(c, x, y)
-  	x, y = math.max(0, x), math.max(0, y)
-  	return ui.Bar(ui.Rect { x = x, y = y, w = ya.clamp(0, area.w - x, 1), h = math.min(1, area.h) }, ui.Bar.TOP)
-  		:symbol(c)
-  end
-
-	return ya.flat {
-
 -- Show symlink path in status bar
 function Status:name()
 	local h = cx.active.current.hovered
