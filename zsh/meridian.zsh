70ed59bebd953e8dea29759684cb0b20cbd2b43d
aafrus <123903734+aafrus@users.noreply.github.com>
2024-07-29 14:24:44 +0200

    Update meridian.zsh

--- zsh/meridian.zsh
+++ zsh/meridian.zsh
@@ -1,22 +1,22 @@
-# Meridian 
-# TTY colors
-
-if [ "$TERM" = "linux" ]; then
-    echo -en "\e]P01e1e1e" # black
-    echo -en "\e]P8585858" # darkgrey
-    echo -en "\e]P19c353e" # darkred
-    echo -en "\e]P9b44c4f" # red
-    echo -en "\e]P27c6f5e" # darkgreen
-    echo -en "\e]PA9e9e9e" # green
-    echo -en "\e]P3d1b57d" # yellow
-    echo -en "\e]PBe2c28f" # light yellow/orange
-    echo -en "\e]P43a3a3a" # darkblue
-    echo -en "\e]PC6a8c9a" # blue
-    echo -en "\e]P59c353e" # darkmagenta
-    echo -en "\e]PDb44c4f" # magenta
-    echo -en "\e]P65e6b6b" # darkcyan
-    echo -en "\e]PE7a8a8a" # cyan
-    echo -en "\e]P7d0d0d0" # lightgrey
-    echo -en "\e]PFf5f5f5" # white
-    clear # for background artifacting
+# futuremyth
+# tty colors
+if [ "$TERM" = "linux" ]
+then
+    echo -en "\e]P0202224" #black
+    echo -en "\e]P83d4047" #darkgrey
+    echo -en "\e]P18d3636" #darkred
+    echo -en "\e]P9813939" #red
+    echo -en "\e]P276b660" #darkgreen
+    echo -en "\e]PA629052" #green
+    echo -en "\e]P3cd6e42" #yellow
+    echo -en "\e]PBb36846" #orange
+    echo -en "\e]P433708c" #darkblue
+    echo -en "\e]PC32657c" #blue
+    echo -en "\e]P5646086" #darkmagenta
+    echo -en "\e]PD535071" #magenta
+    echo -en "\e]P662a8b2" #darkcyan
+    echo -en "\e]PE58929b" #cyan
+    echo -en "\e]P7dceef1" #lightgrey
+    echo -en "\e]PFbeced0" #white
+    clear #for background artifacting
 fi
